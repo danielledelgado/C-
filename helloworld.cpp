f7edd7c10877d06ff13c34f98d0b58dce35138d7
danielledelgado <159454187+danielledelgado@users.noreply.github.com>
2024-09-10 10:39:21 -0400

    Add files via upload

---
+++ helloworld.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int a;
+float b;
+char c;
+bool d;
+string e;
+
+int main()
+{
+    int a = 5;
+    float b = 10.244;
+    char c = 'a';
+    bool d = true;
+    string e = "hello world";
+
+    cout << "An example of an int would be " << a << endl;
+    cout << "An example of a float would be " << b << endl;
+    cout << "An example of a char would be " << c << endl;
+    cout << "An example of a bool would be " << d << endl;
+    cout << "An example of a string would be " << e << endl;
+}
